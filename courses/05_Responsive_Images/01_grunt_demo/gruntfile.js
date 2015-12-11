5d3ec9663d04c09adb5d5704a14b04f38291a888
“leopoldkwok” <leopold.kwok@cohaesus.co.uk>
2015-12-11 15:21:31 +0000

    Learning Grunt

---
+++ courses/05_Responsive_Images/01_grunt_demo/gruntfile.js
@@ -0,0 +1,16 @@
+<!-- The gruntfile.js skeleton -->
+
+<!-- module.exports = function(grunt) {
+	
+	// Configure task(s)
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'), 
+
+	});
+
+	// Load the plugins
+	grunt.loadNpmTasks();
+
+	// Register task(s)
+	grunt.registerTask('default', []);
+} -->
