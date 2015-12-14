9130d8434044d3141be71e0924fd2cc90fa46ccf
“leopoldkwok” <leopold.kwok@cohaesus.co.uk>
2015-12-14 17:39:04 +0000

    Using replace methods

--- courses/06 Javascript Basics/01_resume_project/js/resumeBuilder.js
+++ courses/06 Javascript Basics/01_resume_project/js/resumeBuilder.js
@@ -1,1 +1,17 @@
-$("#main").append("Leopold Kwok");
+// $("#main").append("Leopold Kwok");
+
+// [string].replace([old], [new])
+
+var email = "cameron@udacity.com";
+
+var newEmail = email.replace("udacity", "gmail");
+
+console.log(email);
+
+console.log(newEmail);
+
+var awesomeThoughts = "My name is Cameron and I am AWESOME!";
+
+var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+
+$("#main").append(funThoughts);
