a907052cfbed217c4c82f35c2fba2366056a4288
“leopoldkwok” <leopold.kwok@cohaesus.co.uk>
2015-12-15 13:26:36 +0000

    4 new objects

--- courses/06 Javascript Basics/01_resume_project/js/resumeBuilder.js
+++ courses/06 Javascript Basics/01_resume_project/js/resumeBuilder.js
@@ -1,90 +1,179 @@
+var bio = {
+	"name":"John Doe",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile":"650-555-5555",
+		"email": "john@example.com",
+		"github":"johndoe",
+		"twitter":"@johndoe",
+		"location":"San Francisco"
+	},
+	"welcomeMessage": "Lorem ipsum dolor sit amet etc etc etc.",
+	"skills":[
+		"awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
+	],
+	"bioPic": "images/fry.jpg"
+}
+
+var education = {
+	"schools": [
+		{
+			"name":"Nova Southeastern University",
+			"city":"Fort Lauderdale, FL",
+			"degree": "Masters",
+			"majors":["CS"],
+			"dates": 2013,
+			"url": "http://example.com"
+		},
+		{
+			"name": "Eckered College",
+			"city": "Saint Petersburg, FL",
+			"degree":"BA",
+			"majors":["CS"],
+			"dates": 2003,
+			"url": "http://example.com"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "JavaScript Crash Course",
+			"school":"Udacity",
+			"dates": 2014,
+			"url": "http:///www.udacity.com/course/ud804"
+		}
+	]
+}
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Planet Express",
+			"title": "Delivery Boy",
+			"dates": "January 3000 - Future",
+			"description": "Who moved my cheese cheesy feet cauliflower cheese. Quesp taleggio when the cheese comes out every everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+		},
+		{
+			"employer": "Panucci's Pizza",
+			"title": "Delivery Boy",
+			"dates": "1998 - December 31, 1999",
+			"description": "Who moved my cheese cheesy feet cauliflower cheese. Quesp taleggio when the cheese comes out every everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "Sample Project 1",
+			"dates": "2014",
+			"description": "Who moved my cheese cheesy feet cauliflower
+				cheese. Quesp taleggio when the cheese comes out every everybody's
+				happy airedale ricotta cheese and wine paneer camembert de
+				normandie. Swiss mozzarella cheese slices feta fromage frais
+				airedale swiss cheesecake. Hard cheese blue castello halloumi
+				parmesan say cheese stinking bishop jarlsberg.",
+			"images":[
+				"https://lh3.ggpht.com/23-sq0p0GqF06YX3BwIYPIXLX_Ma_clLXySKEHlphqlxr21-
+				PPbC80U8SjDi96KTWbNjKfY2Pdq_gyFK9A=s300#w=1757&h=1080",
+				"https://lh3.ggpht.com/23-sq0p0GqF06YX3BwIYPIXLX_Ma_clLXySKEHlphqlxr21-
+				PPbC80U8SjDi96KTWbNjKfY2Pdq_gyFK9A=s300#w=1757&h=1080"
+			]
+		}
+	]
+}
+
+
+
+
+
 // $("#main").append("Leopold Kwok");
 
 // [string].replace([old], [new])
 
 // var name = "James";
 
-var email = "cameron@udacity.com";
+// var email = "cameron@udacity.com";
 
-var newEmail = email.replace("udacity", "gmail");
+// var newEmail = email.replace("udacity", "gmail");
 
-console.log(email);
+// console.log(email);
 
-console.log(newEmail);
+// console.log(newEmail);
 
-var awesomeThoughts = "My name is Cameron and I am AWESOME!";
+// var awesomeThoughts = "My name is Cameron and I am AWESOME!";
 
-var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+// var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
 
-$("#main").append(funThoughts);
-
-
-
-var name = "Leopold";
-
-var role = "Junior Web Developer";
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-$("#header").prepend(formattedRole);
-
-$("#header").prepend(formattedName);
-
-var skills = ["awesomeness", "programming", "teaching", "JS"];
-
-// $("#main").append(skills);
-
-// $("#main").append(skills[0]);
-
-$("#main").append(skills.length);
+// $("#main").append(funThoughts);
 
 
-// object declaration
 
-var bio = {
-	"name" : "Leopold Kwok",
-	"role" : "Junior Web Developer",
-	"contacts": {
-		"mobile": "0778-999-5094",
-		"email": "leopoldkwok@gmail.com",
-		"github": "leopoldkwok",
-		"twitter": "@leopoldkwok",
-		"location": "London"
-	},
-	"welcome message" : "Hello World!",
-	"skills": [
-		"awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
-	],
-	"bioPic": "images/fry.jpg"
-};
+// var name = "Leopold";
 
-// $("#main").append(bio.name);
+// var role = "Junior Web Developer";
 
-// bio.city = "Mountain View";
-// bio.email = "leopold.kwok@gmail.com";
+// var formattedName = HTMLheaderName.replace("%data%", name);
 
-bio["city"] = "Mountain View";
-bio["email"] = "james.williams@udacity.com";
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#main").append(bio.city);
+// $("#header").prepend(formattedRole);
 
-// Practice with Object Syntax
+// $("#header").prepend(formattedName);
 
-// Dot notation
+// var skills = ["awesomeness", "programming", "teaching", "JS"];
 
-var work = {};
-work.position = "Course Developer";
-work.employer = "Udacity";
-work.years = 0.3;
+// // $("#main").append(skills);
 
-// bracket notation
+// // $("#main").append(skills[0]);
 
-var education = {};
-education["name"] = "Nova Southeastern University";
-education["years"] = "2005-2013";
-education["city"] = "Fort Lauderdale, FL, US";
+// $("#main").append(skills.length);
+
+
+// // object declaration
+
+// var bio = {
+// 	"name" : "Leopold Kwok",
+// 	"role" : "Junior Web Developer",
+// 	"contacts": {
+// 		"mobile": "0778-999-5094",
+// 		"email": "leopoldkwok@gmail.com",
+// 		"github": "leopoldkwok",
+// 		"twitter": "@leopoldkwok",
+// 		"location": "London"
+// 	},
+// 	"welcome message" : "Hello World!",
+// 	"skills": [
+// 		"awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
+// 	],
+// 	"bioPic": "images/fry.jpg"
+// };
+
+// // $("#main").append(bio.name);
+
+// // bio.city = "Mountain View";
+// // bio.email = "leopold.kwok@gmail.com";
+
+// bio["city"] = "Mountain View";
+// bio["email"] = "james.williams@udacity.com";
+
+// $("#main").append(bio.city);
+
+// // Practice with Object Syntax
+
+// // Dot notation
+
+// var work = {};
+// work.position = "Course Developer";
+// work.employer = "Udacity";
+// work.years = 0.3;
+
+// // bracket notation
+
+// var education = {};
+// education["name"] = "Nova Southeastern University";
+// education["years"] = "2005-2013";
+// education["city"] = "Fort Lauderdale, FL, US";
 
 
 
@@ -109,32 +198,32 @@ education["city"] = "Fort Lauderdale, FL, US";
 // $("#main").append(education.name);
 
 
-var education = {
-	"schools": [
-		{
-			"name": "Nova Southeastern University",
-			"location": "Fort Lauderdale, FL",
-			"degree":"Masters",
-			"majors":["CS"],
-			"dates": 2013,
-			"url": "http://example.com"
-		},
-		{
-			"name":"Eckerd College",
-			"location": "Saint Petersburg, FL",
-			"degree": "BA",
-			"majors":["CS"],
-			"dates": 2003,
-			"url":"http://example.com"
-		}
-	]
-,
-	"onlineCourses": [
-		{
-			"title":"Javascript Syntax",
-			"school":"Udacity",
-			"dates": 2014,
-			"url": "http://www.udacity.com/course/ud804"
-		}
-	]
-}
+// var education = {
+// 	"schools": [
+// 		{
+// 			"name": "Nova Southeastern University",
+// 			"location": "Fort Lauderdale, FL",
+// 			"degree":"Masters",
+// 			"majors":["CS"],
+// 			"dates": 2013,
+// 			"url": "http://example.com"
+// 		},
+// 		{
+// 			"name":"Eckerd College",
+// 			"location": "Saint Petersburg, FL",
+// 			"degree": "BA",
+// 			"majors":["CS"],
+// 			"dates": 2003,
+// 			"url":"http://example.com"
+// 		}
+// 	]
+// ,
+// 	"onlineCourses": [
+// 		{
+// 			"title":"Javascript Syntax",
+// 			"school":"Udacity",
+// 			"dates": 2014,
+// 			"url": "http://www.udacity.com/course/ud804"
+// 		}
+// 	]
+// }
