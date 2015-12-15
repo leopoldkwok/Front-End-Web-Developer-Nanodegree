18f1a64660ae15114b6a50bba927dd999b6ae733
“leopoldkwok” <leopold.kwok@cohaesus.co.uk>
2015-12-15 10:37:00 +0000

    Dot and Bracket Notation

--- courses/06 Javascript Basics/01_resume_project/js/resumeBuilder.js
+++ courses/06 Javascript Basics/01_resume_project/js/resumeBuilder.js
@@ -40,13 +40,36 @@ var skills = ["awesomeness", "programming", "teaching", "JS"];
 
 $("#main").append(skills.length);
 
+
+// object declaration
+
 var bio = {
-	"name" : "James",
-	"age" : 32,
-	"skills" : skills
+	"name" : "Leopold Kwok",
+	"role" : "Junior Web Developer",
+	"contacts": {
+		"mobile": "0778-999-5094",
+		"email": "leopoldkwok@gmail.com",
+		"github": "leopoldkwok",
+		"twitter": "@leopoldkwok",
+		"location": "London"
+	},
+	"welcome message" : "Hello World!",
+	"skills": [
+		"awesomeness", "delivering things", "cryogenic sleep", "saving the universe"
+	],
+	"bioPic": "images/fry.jpg"
 };
 
-$("#main").append(bio.name);
+// $("#main").append(bio.name);
+
+// bio.city = "Mountain View";
+// bio.email = "leopold.kwok@gmail.com";
+
+bio["city"] = "Mountain View";
+bio["email"] = "james.williams@udacity.com";
+
+$("#main").append(bio.city);
+
 
 
 
