caca17e0d402bc989661333dbf57de5500ce1001
“leopoldkwok” <leopold.kwok@cohaesus.co.uk>
2015-12-15 23:38:00 +0000

    Work

--- courses/06 Javascript Basics/01_resume_project/js/resumeBuilder.js
+++ courses/06 Javascript Basics/01_resume_project/js/resumeBuilder.js
@@ -112,6 +112,22 @@ while(cameron.job === "course dev") {
 
 console.log(cameron.job);
 
+for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+	$(".work-entry:last").append(formattedEmployerTitle);
+
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	$(".work-entry:last").append(formattedDates);
+
+	var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	$(".work-entry:last").append(formattedDescription);
+}
+
 
 
 
