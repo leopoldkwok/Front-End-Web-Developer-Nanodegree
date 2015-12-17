af179f344d283b6a5b4f62727a856389a6ee8544
“leopoldkwok” <leopold.kwok@cohaesus.co.uk>
2015-12-17 22:48:07 +0000

    corrected jquery statements

--- courses/07 Intro_to_jQuery/01_modifying_css/app.js
+++ courses/07 Intro_to_jQuery/01_modifying_css/app.js
@@ -7,4 +7,8 @@ You must use jQuery's css() method!
 // Start with this variable!
 var articleItems;
 
-articleItems = // your code goes here!
+// your code goes here!
+
+articleItems = $('.article-item');
+
+articleItems.css('font-size','20px');
