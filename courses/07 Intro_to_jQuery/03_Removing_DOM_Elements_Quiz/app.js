d37df527a1593c6383e60735937070cccf63ccd7
“leopoldkwok” <leopold.kwok@cohaesus.co.uk>
2015-12-18 11:04:04 +0000

    Removing DOM Elements Quiz

---
+++ courses/07 Intro_to_jQuery/03_Removing_DOM_Elements_Quiz/app.js
@@ -0,0 +1,21 @@
+/*
+For this quiz, remove the <ul> from the first article item!
+
+You must use jQuery's remove() method.
+*/
+
+// Start with this variable!
+var articleItems;
+
+articleItems = $('.article-item').first().children('ul').remove(); // your code goes here!
+
+
+// Udacity solution
+
+// var articleItems, ul;
+
+// articleItems = $('.article-item');
+
+// ul = articleItems.find('ul');
+
+// ul.remove();
