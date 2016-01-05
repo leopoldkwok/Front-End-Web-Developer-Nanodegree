7046727b5c6031d9f2b55c1ea2cee447b77d3b07
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-05 15:07:20 +0000

    instanceof_operator

---
+++ courses/09_object_oriented_javascript/04_Prototypal_Classes/02_instanceof_operator/script.js
@@ -0,0 +1,7 @@
+var Dog = function() {
+	return {legs: 4, bark: alert}; // written in functional style - object literal so delegates to object.prototype
+};
+
+var fido = Dog();
+
+console.log(fido instanceof Dog);
