0120e6ca8d5a06affb95b988f46749b3206ba121
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-05 16:27:13 +0000

    Intro to Pseudoclassical Subclasses

---
+++ courses/09_object_oriented_javascript/07_Pseudoclassical_Subclasses/01_Pseudoclassical_Subclasses/library.js
@@ -0,0 +1,10 @@
+// Car function
+
+var Car = function(loc) {
+	this.loc = loc;
+};
+
+// prototype chains is good for saving memory
+Car.prototype.move = function() {
+	this.loc++;
+};
---
+++ courses/09_object_oriented_javascript/07_Pseudoclassical_Subclasses/01_Pseudoclassical_Subclasses/run.js
@@ -0,0 +1,7 @@
+var zed = new Car(3); // car function required to make instances of cars
+zed.move();
+
+var amy = new Van(9);
+amy.move();
+amy.grab();
+
