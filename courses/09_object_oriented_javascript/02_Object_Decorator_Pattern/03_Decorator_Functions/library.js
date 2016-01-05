7b616ede3746880d79a63f7654dd7dd32278a4b1
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-05 10:33:16 +0000

    Decorator Functions

---
+++ courses/09_object_oriented_javascript/02_Object_Decorator_Pattern/03_Decorator_Functions/library.js
@@ -0,0 +1,11 @@
+var carlike = function(obj, loc) { // set the car location
+
+	/* ...? */
+	obj.loc = loc;
+	return obj;
+};
+
+var move = function(car) {
+	car.loc++;
+};
+
---
+++ courses/09_object_oriented_javascript/02_Object_Decorator_Pattern/03_Decorator_Functions/run.js
@@ -0,0 +1,8 @@
+// global variable
+
+// The in-memory model diagram surrounds numbers with brackets, turning 1 into {1} to further emphasize the fact that everything is an object in JavaScript.
+var amy = carlike{{}, loc:1};
+move(amy);
+// more enemies
+var ben = carlike{{}, loc:9};
+move(ben);
