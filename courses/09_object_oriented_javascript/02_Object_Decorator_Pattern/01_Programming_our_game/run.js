f776c7a438163c89ff2182b897db12781453727b
“leopoldkwok” <leopoldkwok@gmail.com>
2016-01-05 10:08:26 +0000

    Programming our game

---
+++ courses/09_object_oriented_javascript/02_Object_Decorator_Pattern/01_Programming_our_game/run.js
@@ -0,0 +1,9 @@
+// global variable
+
+// The in-memory model diagram surrounds numbers with brackets, turning 1 into {1} to further emphasize the fact that everything is an object in JavaScript.
+var amy = {loc:1};
+amy.loc++;
+// more enemies
+
+var ben = {loc:9};
+ben.loc++;
