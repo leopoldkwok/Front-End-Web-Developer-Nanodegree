7d92868e4176e8bc832a561c9f36541a9b801552
“leopoldkwok” <leopoldkwok@gmail.com>
2016-04-11 14:43:52 +0100

    NYT Implementation

--- courses/11_intro_to_ajax/minicourse-ajax-project/js/script.js
+++ courses/11_intro_to_ajax/minicourse-ajax-project/js/script.js
@@ -25,6 +25,25 @@ function loadData() {
 
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    // Your NYTimes AJAX request goes here
+
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='
+    + cityStr + '&sort=newest&api-key=55bb2384d8504ced1a598513b5db1f52:7:74974531'
+
+    $.getJSON(nytimesUrl, function(data) {
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_url+'">'+article.headline.main+'</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        };
+    });
+
+
+
 
     return false;
 };
