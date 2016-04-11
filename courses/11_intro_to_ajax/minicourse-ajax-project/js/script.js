5bfd32bee5919e613181093944e1e2259d7ac256
“leopoldkwok” <leopoldkwok@gmail.com>
2016-04-11 12:44:51 +0100

    loading streeview

--- courses/11_intro_to_ajax/minicourse-ajax-project/js/script.js
+++ courses/11_intro_to_ajax/minicourse-ajax-project/js/script.js
@@ -15,6 +15,17 @@ function loadData() {
 
     // YOUR CODE GOES HERE!
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ',' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
     return false;
 };
 
