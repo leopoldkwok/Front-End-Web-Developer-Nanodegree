2482bb89789bd61d14d23b2f5d326ef1daffcf99
“leopoldkwok” <leopoldkwok@gmail.com>
2016-05-02 23:30:45 +0100

    Starting to add the filter functionality

--- FE_Nanodegree_Project_5/js/app.js
+++ FE_Nanodegree_Project_5/js/app.js
@@ -29,21 +29,53 @@ var Place = function(data) {
   this.latLang = ko.observable(data.latLang);
 };
 
-var ViewModel = function(data) {
-  var self = this;
-  this.placeList = ko.observableArray([]);
+var placeFilters = [
+  {
+    Type: "text",
+    Name: "Name",
+    Value: ko.observable(""),
+    RecordValue: function(record) { return record.name;}
+  }
+];
 
-  places.forEach(function(aplace){
-    self.placeList.push(new Place(aplace));
-  });
-
-  this.currentPlace = ko.observable('London');
-
-  this.setPlace = function(chosenPlace) {
-    self.currentPlace(chosenPlace);
+// This initializes the Google Maps section, and puts markers for all the places
+initialize = function() {
+  var mapCanvas = document.getElementById('map-canvas');
+  // Co-ordinates of Central London
+  var myLatLng = new google.maps.LatLng(51.5067, -0.1428);
+  var mapOptions = {
+    center: myLatLng,
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
-  this.loadData = function() {
+  var map = new google.maps.Map(mapCanvas,mapOptions);
+
+  var contentString = 'You clicked this';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var numPlaces = places.length;
+  for (i = 0; i < numPlaces; i++) {
+    var marker = new google.maps.Marker({
+      position: places[i].latLang,
+      map: map,
+      title: places[i].name
+    });
+
+    google.maps.event.addListener(marker,'click',(function(markerCopy){
+      return function(){
+        infowindow.open(map, markerCopy);
+      };
+    })(marker));
+  };
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+// Load the Wikipedia articles when user searches for a place
+loadData = function() {
     var $wikiElem = $('#wikipedia-links');
     var $greeting = $('#greeting');
 
@@ -78,43 +110,93 @@ var ViewModel = function(data) {
   return false;
 };
 
+$('#form-container').submit(loadData);
 
-$('#form-container').submit(this.loadData);
+var ViewModel = function(data) {
+  var self = this;
+  this.placeList = ko.observableArray([]);
 
-// This initializes the Google Maps section, and puts markers for all the places
-this.initialize = function() {
-  var mapCanvas = document.getElementById('map-canvas');
-  // Co-ordinates of Central London
-  var myLatLng = new google.maps.LatLng(51.5072, -0.1276);
-  var mapOptions = {
-    center: myLatLng,
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  var map = new google.maps.Map(mapCanvas,mapOptions);
-
-  var contentString = 'You clicked this';
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
+  places.forEach(function(aplace){
+    self.placeList.push(new Place(aplace));
   });
 
-  var numPlaces = places.length;
-  for (i = 0; i < numPlaces; i++) {
-    var marker = new google.maps.Marker({
-      position: places[i].latLang,
-      map: map,
-      title: places[i].name
-    });
+  this.currentPlace = ko.observable('London');
 
-    google.maps.event.addListener(marker,'click',(function(markerCopy){
-      return function(){
-        infowindow.open(map, markerCopy);
-      };
-    })(marker));
+  this.setPlace = function(chosenPlace) {
+    self.currentPlace(chosenPlace);
   };
-};
-  google.maps.event.addDomListener(window, 'load', this.initialize);
+
+  self.filter = new FilterModel(placeFilters, this.placeList());
 }
+
+var FilterModel = function(filters, records) {
+  var self = this;
+  self.records = ko.observableArray(records);
+  self.filters = ko.observableArray(filters);
+  self.activeFilters = ko.computed(function() {
+    var filters = self.filters();
+    var activeFilters = [];
+    for (var index = 0; index < filters.length; index++)
+    {
+      var filter = filters[index];
+      if(filters.Value)
+      {
+        var filterValue = filters.Value();
+        if(filterValue && filterValue !="")
+        {
+          var activeFilter = {
+            Filter: filters,
+            IsFiltered: function(filters, record)
+            {
+              var filterValue = filters.Value();
+              filterValue = filterValue.toUpperCase();
+
+              var recordValue = filters.RecordValue(record);
+              console.log("logging");
+              console.log(recordValue);
+              recordValue = recordValue.toUpperCase();
+              return recordValue.indexOf(filterValue) == -1;
+            }
+          };
+          activeFilters.push(activeFilter);
+        }
+      }
+    }
+    return activeFilters;
+  });
+  self.filteredRecords = ko.computed(function() {
+    var records = self.records();
+    var filters = self.activeFilters();
+    if (filters.length == 0)
+    {
+      return records;
+    }
+
+    var filteredRecords = [];
+    for (var rIndex = 0; rIndex < records.length; rIndex++)
+    {
+      var isIncluded = true;
+      var record = records[rIndex];
+      for(var index = 0; fIndex < filters.length; fIndex++)
+      {
+        var filter = filters[fIndex];
+
+        var isFiltered = filter.IsFiltered(filter.Filter, record);
+        if(isFiltered)
+        {
+          isIncluded = false;
+          break;
+        }
+    }
+    if(isIncluded)
+    {
+      filteredRecords.push(record);
+    }
+  }
+    return filteredRecords;
+  }).extend({throttle: 200});
+
+}
+
 
 ko.applyBindings(new ViewModel());
