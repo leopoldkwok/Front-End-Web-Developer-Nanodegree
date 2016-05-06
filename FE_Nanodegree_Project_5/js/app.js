f2f22721b12a9710d773d013417208b259deaceb
“leopoldkwok” <leopoldkwok@gmail.com>
2016-05-06 06:36:51 +0100

    Amended code and added comments

--- FE_Nanodegree_Project_5/js/app.js
+++ FE_Nanodegree_Project_5/js/app.js
@@ -1,97 +1,45 @@
-// Points of interest saved in an array
+// This is the Data Model, each data point has a name and latlong co-ordinates
 
-var places = [
-  {
-    name: 'Big Ben',
-    latLang: new google.maps.LatLng(51.500956, -0.124582)
-  },
-  {
-    name: 'Tower Bridge',
-    latLang: new google.maps.LatLng(51.505650, -0.075389)
-
-  },
-  {
-    name: 'Tower of London',
-    latLang: new google.maps.LatLng(51.508286, -0.075949)
-  },
-  {
-    name: 'Westminister Abbey',
-    latLang: new google.maps.LatLng(51.499492, -0.127342)
-  },
-  {
-    name: 'London Eye',
-    latLang: new google.maps.LatLng(51.503511, -0.119564)
-  }];
-
-// This is the model
 var Place = function(data) {
-  this.name = ko.observable(data.name);
-  this.latLang = ko.observable(data.latLang);
+  var self = this;
+  self.name = data.name;
+  self.lat = data.lat;
+  self.lon = data.lon;
 };
 
-var placeFilters = [
-  {
-    Type: "text",
-    Name: "Name",
-    Value: ko.observable(""),
-    RecordValue: function(record) { return record.name;}
-  }
-];
+// This is the ViewModel
 
-// This initializes the Google Maps section, and puts markers for all the places
-initialize = function() {
-  var mapCanvas = document.getElementById('map-canvas');
-  // Co-ordinates of Central London
-  var myLatLng = new google.maps.LatLng(51.5067, -0.1428);
-  var mapOptions = {
-    center: myLatLng,
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+var PlacePageModel = function(data) {
+  var self = this;
+  this.placeList = ko.observableArray([]);
 
-  var map = new google.maps.Map(mapCanvas,mapOptions);
-
-  var contentString = 'You clicked this';
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
+  places.forEach(function(aplace){
+    self.placeList.push(new Place(aplace));
   });
 
-  var numPlaces = places.length;
-  for (i = 0; i < numPlaces; i++) {
-    var marker = new google.maps.Marker({
-      position: places[i].latLang,
-      map: map,
-      title: places[i].name
-    });
+  this.currentPlace = ko.observable('');
 
-    google.maps.event.addListener(marker,'click',(function(markerCopy){
-      return function(){
-        infowindow.open(map, markerCopy);
-      };
-    })(marker));
+  this.setPlace = function(chosenPlace) {
+    self.currentPlace(chosenPlace);
   };
-};
-
-google.maps.event.addDomListener(window, 'load', initialize);
 
 // Load the Wikipedia articles when user searches for a place
-loadData = function() {
-    var $wikiElem = $('#wikipedia-links');
-    var $greeting = $('#greeting');
+this.loadData = function() {
+  var $wikiElem = $('#wikipedia-links');
+  var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
+  // clear out old data before new request
+  $wikiElem.text("");
 
-    var poiStr = $('#poi').val();
-    $greeting.text('So, you want to explore at ' + poiStr + '?');
-
-  // Wikipedia stuff
+  var poiStr = String(place.name);
   var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + poiStr + '&format=json&callback=wikiCallback';
 
+  // Error Handling, this will show a text message if articles fail to load
   var wikiRequestTimeout = setTimeout(function() {
-    $wikiElem.text("failed to get wikipedia resources");
+  $wikiElem.text("failed to get wikipedia resources");
   }, 5000);
 
+  // Make an ajax call to wikiURL and append results to DOM wiki element
 
   $.ajax({
     url: wikiUrl,
@@ -106,97 +54,174 @@ loadData = function() {
       };
       clearTimeout(wikiRequestTimeout);
     }
-  })
+  });
   return false;
 };
 
-$('#form-container').submit(loadData);
+// Create filters for the place list
+var filters = [
+  {
+    Type: "text",
+    Name: "Name",
+    Value: ko.observable(""),
+    RecordValue: function(record) { return record.name;}
+  }];
 
-var ViewModel = function(data) {
+  self.filter = new Filter(filters, this.placeList);
+  self.pager = new PagerModel(self.filter.filteredRecords);
+};
+
+// Model that represents the returned records from filtering
+
+var PagerModel = function(records) {
   var self = this;
-  this.placeList = ko.observableArray([]);
+  self.records = GetObservableArray(records);
 
-  places.forEach(function(aplace){
-    self.placeList.push(new Place(aplace));
-  });
+  self.currentPageRecords = ko.computed(function() {
+    return self.records();
+  }).extend({ throttle: 5});
+};
 
-  this.currentPlace = ko.observable('London');
+// Compare values of search and place names and only return the matching ones as filtered
 
-  this.setPlace = function(chosenPlace) {
-    self.currentPlace(chosenPlace);
-  };
-
-  self.filter = new FilterModel(placeFilters, this.placeList());
-}
-
-var FilterModel = function(filters, records) {
+var Filter = function(filters, records) {
   var self = this;
-  self.records = ko.observableArray(records);
+  self.records = GetObservableArray(records);
   self.filters = ko.observableArray(filters);
+  // Compare the name of each place to the user input text string
+  // If they match, mark the place as having passed the filter
   self.activeFilters = ko.computed(function() {
     var filters = self.filters();
     var activeFilters = [];
-    for (var index = 0; index < filters.length; index++)
-    {
+    for (var index = 0; index < filters.length; index++) {
       var filter = filters[index];
-      if(filters.Value)
-      {
-        var filterValue = filters.Value();
-        if(filterValue && filterValue !="")
-        {
+      if(filter.Value) {
+        var filterValue = filter.Value();
+        if(filterValue && filterValue !="") {
           var activeFilter = {
-            Filter: filters,
-            IsFiltered: function(filters, record)
+            Filter: filter,
+            IsFiltered: function(filter, record)
             {
-              var filterValue = filters.Value();
+              var filterValue = filter.Value();
               filterValue = filterValue.toUpperCase();
 
-              var recordValue = filters.RecordValue(record);
-              console.log("logging");
-              console.log(recordValue);
+              var recordValue = filter.RecordValue(record);
               recordValue = recordValue.toUpperCase();
               return recordValue.indexOf(filterValue) == -1;
             }
           };
           activeFilters.push(activeFilter);
-        }
-      }
-    }
+        };
+      };
+    };
     return activeFilters;
   });
+
+  // Go through each record in the list and find whether it was filtered from the previous step
+  // If it is filtered, do not include in the new list that gets returned to the view
+
   self.filteredRecords = ko.computed(function() {
     var records = self.records();
     var filters = self.activeFilters();
-    if (filters.length == 0)
-    {
-      return records;
-    }
-
     var filteredRecords = [];
-    for (var rIndex = 0; rIndex < records.length; rIndex++)
-    {
+    for (var rIndex = 0; rIndex < records.length; rIndex++) {
       var isIncluded = true;
       var record = records[rIndex];
-      for(var index = 0; fIndex < filters.length; fIndex++)
-      {
+      for(var fIndex = 0; fIndex < filters.length; fIndex++) {
         var filter = filters[fIndex];
-
         var isFiltered = filter.IsFiltered(filter.Filter, record);
-        if(isFiltered)
-        {
+        if(isFiltered) {
           isIncluded = false;
           break;
-        }
-    }
-    if(isIncluded)
-    {
+        };
+    };
+    if(isIncluded) {
       filteredRecords.push(record);
-    }
-  }
-    return filteredRecords;
+    };
+  };
+  return filteredRecords;
   }).extend({throttle: 200});
 
-}
+};
+
+// Method to create ko.observableArray even if original input is not an array
+var GetObservableArray = function(array) {
+  if(typeof(array) == 'function'); {
+    return array;
+  };
+  return ko.observableArray(array);
+};
+
+// Save all the points of interest
+
+var places = [
+  {
+    name: 'Big Ben',
+    lat: 51.500956,
+    lon: -0.124582
+  },
+  {
+    name: 'Tower Bridge',
+    lat: 51.505650,
+    lon: -0.075389
+  },
+  {
+    name: 'Tower of London',
+    lat: 51.508286,
+    lon: -0.075949
+  },
+  {
+    name: 'Westminister Abbey',
+    lat: 51.499492,
+    lon: -0.127342
+  },
+  {
+    name: 'London Eye',
+    lat: 51.503511,
+    lon: -0.119564
+}];
+
+ko.applyBindings(new PlacePageModel(places));
 
 
-ko.applyBindings(new ViewModel());
+// This initializes the Google Maps section, and puts markers for all the places
+var initialize = function() {
+  var mapCanvas = document.getElementById('map-canvas');
+  // Co-ordinates of Central London
+  var myLatLng = new google.maps.LatLng(51.5067, -0.1428);
+  var mapOptions = {
+    center: myLatLng,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(mapCanvas,mapOptions);
+
+  var contentString = 'You clicked this';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  // Place a marker on each point of interest
+  var numPlaces = places.length;
+  for (i = 0; i < numPlaces; i++) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(places[i].lat, places[i].lon),
+      map: map,
+      title: places[i].name
+    });
+    // Add functionality to differentiate clicked vs unclicked markers
+    google.maps.event.addListener(marker,'click',(function(markerCopy){
+      return function(){
+        infowindow.open(map, markerCopy);
+      };
+    })(marker));
+  };
+};
+
+// When google maps fails to load, user will see this error message
+try {
+  google.maps.event.addDomListener(window,'load', initialize);
+} catch(err) {
+  document.getElementById("map-canvas").innerHTML = 'Sorry, cannot load google map';
+};
